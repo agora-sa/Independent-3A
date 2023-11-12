732933e1f2ab62fbf054eaabe1d997937dc7b7e7
wangwei02 <wangwei02@agora.io>
2023-11-12 21:05:57 +0800

    upload app icon

--- app/src/main/java/io/agora/ainoise/MainActivity.java
+++ app/src/main/java/io/agora/ainoise/MainActivity.java
@@ -2,6 +2,8 @@ package io.agora.ainoise;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,14 +14,23 @@ import io.agora.ainoise.activity.FileRawActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String appId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        appId = BuildConfig.AGORA_APP_ID;
 
         findViewById(R.id.ains_for_trtc).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, TRTCRawEnterActivity.class)));
 
-        findViewById(R.id.ains_for_agora).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, AgoraRawActivity.class)));
+        findViewById(R.id.ains_for_agora).setOnClickListener(v -> {
+            if ("".equals(appId)) {
+                Toast.makeText(MainActivity.this, "请到gradle.properties中填写APPID等信息", Toast.LENGTH_LONG).show();
+                return;
+            }
+            startActivity(new Intent(MainActivity.this, AgoraRawActivity.class));
+        });
 
         findViewById(R.id.ains_for_source_file).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, FileRawActivity.class)));
     }
--- app/src/main/java/io/agora/ainoise/activity/AgoraRawActivity.java
+++ app/src/main/java/io/agora/ainoise/activity/AgoraRawActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatButton;
